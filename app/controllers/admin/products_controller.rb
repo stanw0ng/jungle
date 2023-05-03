90509c01e82dacc435485d1b4e3251b60955b323
stanw0ng <user.stanley@gmail.com>
2023-05-03 18:47:45 -0400

    add HTTP basic auth

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,5 @@
 class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV['AUTH_USERNAME'].to_s, password: ENV['AUTH_PASSWORD'].to_s
 
   def index
     @products = Product.order(id: :desc).all
